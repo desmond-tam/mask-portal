c03ab01f84eda594960b84903876250a8c47e381
Desmond Tam <desmondeds@hotmail.com>
2022-09-26 22:57:59 +1000

    remove header option

--- src/components/header-section.tsx
+++ src/components/header-section.tsx
@@ -130,9 +130,9 @@ export const HeaderSection = (props:any) => {
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="#features">Features</Nav.Link>
-                  <Nav.Link href="#pricing">Pricing</Nav.Link>
-                  <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                  {/* <Nav.Link href="#features">Features</Nav.Link>
+                  <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+                  {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">
                       Another action
@@ -142,7 +142,7 @@ export const HeaderSection = (props:any) => {
                     <NavDropdown.Item href="#action/3.4">
                       Separated link
                     </NavDropdown.Item>
-                  </NavDropdown>
+                  </NavDropdown> */}
                 </Nav>
                 <Nav>
                   <Nav.Link href="#deets">Login</Nav.Link>
